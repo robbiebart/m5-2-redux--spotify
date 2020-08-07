8062c91755f717e8951bed889e049a65ca54ea98
Robbie Bart <adm@AdminisatorsAir.lan>
2020-08-07 10:23:59 -0400

    5

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import GlobalStyles from "../GlobalStyles";
+
+const DEFAULT_ARTIST_ID = "4Z8W4fKeB5YxbusRsdQVPb";
 
 const App = () => {
-  return <Router>TODO</Router>;
+  return (
+    <>
+      <Router>TODO</Router>
+      <GlobalStyles />
+    </>
+  );
 };
 
 export default App;
